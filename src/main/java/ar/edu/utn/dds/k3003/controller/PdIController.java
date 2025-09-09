0560f4090a33002af74925f5c305d76676d0f1b9
Sebabalas <sbals@frba.utn.edu.ar>
2025-09-09 11:39:37 -0300

    agrego mensaje de bienvenida

--- src/main/java/ar/edu/utn/dds/k3003/controller/PdIController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/PdIController.java
@@ -49,4 +49,9 @@ public class PdIController {
     public ResponseEntity<PdIDTO> procesarNuevoPdi(@RequestBody PdIDTO pdi) {
         return ResponseEntity.ok(fachadaProcesadorPdI.procesar(pdi));
     }
+    // GET / -> mensaje en la página
+    @GetMapping("/")
+    public String home() {
+        return "¡La app está corriendo!";
+}
 }
