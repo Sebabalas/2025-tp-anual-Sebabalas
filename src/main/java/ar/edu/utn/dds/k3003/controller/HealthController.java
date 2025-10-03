1c514e6bd4fd09c9590c7e88366d3a849efa5cd4
Franco di Napoli <franco.dinapoli@tiendanube.com>
2025-10-03 19:26:48 -0300

    health controller

--- src/main/java/ar/edu/utn/dds/k3003/controller/HealthController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/HealthController.java
@@ -3,6 +3,7 @@ package ar.edu.utn.dds.k3003.controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/health")
 public class HealthController {
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, String>> root() {
         return ResponseEntity.ok(Map.of("status", "OK"));
     }
