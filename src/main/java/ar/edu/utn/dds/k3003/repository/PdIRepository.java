cdf8435ed1beb4f24bf87a007856dc6ca935416d
Franco di Napoli <franco.dinapoli@tiendanube.com>
2025-11-14 00:34:36 -0300

    cleanup

--- src/main/java/ar/edu/utn/dds/k3003/repository/PdIRepository.java
+++ src/main/java/ar/edu/utn/dds/k3003/repository/PdIRepository.java
@@ -1,13 +1,11 @@
 package ar.edu.utn.dds.k3003.repository;
 
 import ar.edu.utn.dds.k3003.model.PdI;
-import ar.edu.utn.dds.k3003.model.EstadoPdi;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.domain.Pageable;
 
 import jakarta.transaction.Transactional;
 
@@ -17,7 +15,6 @@ public interface PdIRepository {
     PdI save(PdI pdi);
     List<PdI> findAll();
     void deleteAll();
-    List<PdI> findByEstadoOrderByIdAsc(EstadoPdi estado, Pageable pageable);
     
     @Transactional
     @Modifying
